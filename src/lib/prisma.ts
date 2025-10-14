55c837b6c670fb501d71e0c578d5d3e121d7373a
hanifski <h@etalas.com>
2025-10-14 22:41:18 +0700

    prisma is now edge runtime compatible

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaNeon } from '@prisma/adapter-neon'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: any
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const createPrismaClient = () => {
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
+  
+  return new PrismaClient({ adapter }).$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
