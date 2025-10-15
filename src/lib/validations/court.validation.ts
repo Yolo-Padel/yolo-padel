56b4e03ea6d9eef2fcc2fc635db235825c86cc85
hanifski <h@etalas.com>
2025-10-15 23:53:42 +0700

    new court table

--- src/lib/validations/court.validation.ts
+++ src/lib/validations/court.validation.ts
@@ -5,6 +5,8 @@ export const courtCreateSchema = z.object({
   isActive: z.boolean(),
   isArchived: z.boolean().default(false),
   venueId: z.string().min(1, "Venue ID is required"),
+  location: z.string().min(1, "Location is required"),
+  images: z.array(z.string().url("Invalid image URL")),
 });
 
 export type CourtCreateInput = z.infer<typeof courtCreateSchema>;
