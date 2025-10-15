b200a3a7157afe985bd4acf5176cc959c6a45122
thufail <t@etalas.com>
2025-10-15 15:49:43 +0700

    tweak auth page layout

--- src/app/admin/auth/layout.tsx
+++ src/app/admin/auth/layout.tsx
@@ -8,14 +8,14 @@ export default function AdminAuthLayout({
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
+        {/* <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
             Yolo Padel
           </a>
-        </div>
+        </div> */}
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             {children}
@@ -28,6 +28,9 @@ export default function AdminAuthLayout({
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
+        <div className="absolute top-10 left-10">
+          <img src="/yolo-black.png" alt="Yolo Logo" className="w-[100px] h-[38px]" />
+        </div>
       </div>
     </div>
   )
