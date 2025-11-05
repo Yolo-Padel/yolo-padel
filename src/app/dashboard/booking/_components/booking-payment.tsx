f648b057e43892700c845b11c4d492097ea92e7b
bagas-b <b@etalas.com>
2025-11-05 19:30:04 +0700

    Feature/user modal (#34)
    * user-dashboard modal
    
    * user-modal-fix
    
    * ..
    
    * fix error when build

---
+++ src/app/dashboard/booking/_components/booking-payment.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import React from "react"
+import {Button} from '@/components/ui/button'
+import { ArrowLeftIcon } from "lucide-react"
+
+type PaymentProps ={
+  id: string;
+  venue: string;
+  courtName: string;
+  image?: string;
+  bookingTime?: string;
+  bookingDate: string;
+  duration: string;
+  totalPayment: number;
+  status: string | "Upcoming" | "Expired" | "Completed";
+  paymentMethod: string | "Credit Card" | "QRIS" | "Bank Transfer";
+  paymentStatus: string | "Paid" | "Unpaid";
+}
+
+export function Payment ({
+    open,
+    onOpenChange,
+    paymentProps,
+    onChangeMode,
+
+}: {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+    paymentProps: PaymentProps | null
+    onChangeMode: (mode: "booking-details" | "order-summary" | "book-again" | "payment-paid" | "payment-pending" | "booking-payment") => void;
+})  {
+
+
+    return (
+
+        <div>
+            <div className="flex items-center gap-4">
+                <Button className="bg-primary" onClick={() => onOpenChange(false)}>
+                    <ArrowLeftIcon className="h-4 w-4 mx-2" />
+                </Button>
+                <div>
+                    <h2 className="text-2xl font-semibold">Finish this Payment</h2>
+                    <span className='font-normal text-sm text-muted-foreground pt-2'>{paymentProps?.bookingDate}</span>
+                </div>
+            </div>
+                <div className='text-xl gap-4 mt-4 space-y-6 items-center'>
+                    <img src="/scan_me_qr_code.jpg" alt="barcode" className='w-[full] h-[full] rounded-md' />
+                
+                    <div className='flex flex-col items-center gap-2 text-sm text-foreground'>
+                        <div>Total Payment Rp {paymentProps?.totalPayment}</div>
+                        <div>Expires in 15 minutes</div>
+                    </div>     
+                </div>
+            <div className='mt-4'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 w-full gap-2'>
+                    <Button 
+                        className ="w-full border-primary rounded-sm"
+                        variant="outline"
+                        onClick={()=>onOpenChange(false)}>
+                        Download QRIS
+                    </Button>
+
+                    {/*Payment Paid*/}
+                    {paymentProps?.paymentStatus === "Paid" && (
+                    <Button 
+                        className ="w-full rounded-sm border-primary"
+                        variant="outline"
+                        onClick={()=>{onChangeMode("payment-paid")}}>
+                        Payment Status
+                    </Button>
+                    )}
+
+                    {/*Payment Unpaid*/}
+                    {paymentProps?.paymentStatus === "Unpaid" && (
+                    <Button 
+                        className ="w-full rounded-sm border-primary"
+                        variant="outline"
+                        onClick={()=>{onChangeMode("payment-pending")}}>
+                        Payment Status
+                    </Button>
+                    )}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/dashboard/booking/page.tsx
+++ src/app/dashboard/booking/page.tsx
@@ -1,10 +1,14 @@
+"use client"
 
 import { BookingCourt } from "@/app/dashboard/booking/_components/booking-court";
+import { useState } from "react";
 
 export default function BookingPage() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <BookingCourt />
+      <BookingCourt open={open} onOpenChange={setOpen} />
     </div>
   )
 }
