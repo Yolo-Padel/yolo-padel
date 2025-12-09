345c802fc723304e8aec667cdcaf6b52b0990af8
thufail <t@etalas.com>
2025-12-09 13:45:41 +0700

    whitelist prod blob

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      /**
+       * @description
+       *  Staging Url
+       */
       {
         protocol: "https",
         hostname: "yh2clvb66mbjivvk.public.blob.vercel-storage.com",
+      },
+      /**
+       * @description
+       *  Production Url
+       */
+      {
+        protocol: "https",
+        hostname: "5n03s4kvfdmgjrwz.public.blob.vercel-storage.com",
       },
     ],
   },
